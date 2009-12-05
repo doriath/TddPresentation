ddc017dbcc367eef44a40a947dbef9b338d5c823
psla <psla@sepio.pl>
2009-12-06 00:10:47 +0100

    Reformatted code

--- Kody/MoqOverview/MoqOverviewTests/ControllerTests/ControllerSecondApproachTests.cs
+++ Kody/MoqOverview/MoqOverviewTests/ControllerTests/ControllerSecondApproachTests.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Moq;
 using MoqOverview;
 using MoqOverview.Repo;
-using MoqOverviewTests.FakeClasses;
 using NUnit.Framework;
 
 namespace MoqOverviewTests.ControllerTests
@@ -12,41 +10,39 @@ namespace MoqOverviewTests.ControllerTests
     [TestFixture]
     public class ControllerSecondApproachTests
     {
+        #region Setup/Teardown
+
+
+        [SetUp]
+        public void SetUp ()
+        {
+            //create result set
+            this.m_employeesList = new List<Employee> ();
+            this.m_employeesList.Add (new Employee ()
+                {
+                        FirstName = "Mój",
+                        Salary = 2000
+                });
+            //approach 2. create moq...
+            var repository = new Mock<IEmployeeRepository> ();
+            repository.Setup (x => x.GetEmployees ()).Returns (this.m_employeesList);
+            this.m_controller = new Controller (repository.Object);
+        }
+
+
+        #endregion
+
+
         private Controller m_controller;
         private List<Employee> m_employeesList;
 
 
-        [SetUp]
-        public void SetUp()
-        {
-            //create result set
-            m_employeesList = new List<Employee> ();
-            m_employeesList.Add(new Employee ()
-                                {
-                                        FirstName = "Mój",
-                                        Salary = 2000
-                                });
-            //approach 2. create moq...
-            var repository = new Mock<IEmployeeRepository> ();
-            repository.Setup (x => x.GetEmployees ()).Returns ( m_employeesList );
-            m_controller = new Controller (repository.Object);
-        }
-
-        [Test]
-        public void WritingUsersTest()
-        {
-            string[] result = this.m_controller.ShowUsers().ToArray();
-
-            Assert.AreEqual("Mój: 2000,00z³", result[0]);
-            Assert.AreEqual(1, result.Length);
-        }
-
         //something cool now..
         [Test]
-        public void WhetherEveryUserHasRiseSalaryCalledTest()
+        public void WhetherEveryUserHasRiseSalaryCalledTest ()
         {
-            var employee = new Mock<Employee>();
-            employee.Setup (x => x.RaiseSalaryLinear (It.IsAny<double> ())).Verifiable();
+            var employee = new Mock<Employee> ();
+            employee.Setup (x => x.RaiseSalaryLinear (It.IsAny<double> ())).Verifiable ();
             var employeesList = new List<Employee>
                 {
                         employee.Object,
@@ -59,10 +55,20 @@ namespace MoqOverviewTests.ControllerTests
             controller.RaiseSalaryOfAllUsers (150.00);
 
             //mozemy sprawdzic, czy byla wywolana tyle razy
-            employee.Verify(x => x.RaiseSalaryLinear (It.IsAny<double> ()), Times.Exactly (2));
-            
+            employee.Verify (x => x.RaiseSalaryLinear (It.IsAny<double> ()), Times.Exactly (2));
+
             //a nawet, czy z konkretnymi argumentami
-            employee.Verify(x => x.RaiseSalaryLinear(It.Is<double>(y => y == 150.0)), Times.Exactly(2));
+            employee.Verify (x => x.RaiseSalaryLinear (It.Is<double> (y => y == 150.0)), Times.Exactly (2));
+        }
+
+
+        [Test]
+        public void WritingUsersTest ()
+        {
+            string[] result = this.m_controller.ShowUsers ().ToArray ();
+
+            Assert.AreEqual ("Mój: 2000,00z³", result[0]);
+            Assert.AreEqual (1, result.Length);
         }
     }
 }
